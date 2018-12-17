db763d37ccb33d9c200229de36e7598aa74252e1
cochiletSerban <cochiletSerban@gmail.com>
2018-12-17 17:31:51 +0200

    added storage object

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+//  includes
 const express = require('express');
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const gridFsStorage = require('multer-gridfs-storage');
+const grid = require('gridfs-stream');
+const mehtodOverrider =  require('method-override');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+//  middleware
+app.use(bodyParser.json());
+app.use(mehtodOverrider('_method'));
 app.set('view engine', 'ejs');
+
+//  mongo uri
+const mongoUri = 'mongodb://test:test123@ds137404.mlab.com:37404/piky';
 
 app.get('/', (req, res) =>{
     res.render('index');
